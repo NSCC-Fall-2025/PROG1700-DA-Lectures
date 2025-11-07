a2df88b15d127e843d35877d2dd522a378b0c42b
Brian Shewan <brian.shewan@nscc.ca>
2025-11-07 12:24:28 -0400

    added unit testing demos

---
+++ test_program_to_test.py
@@ -0,0 +1,12 @@
+
+def test_number_assumptions():
+    age = 25
+
+    # check to see if the age is a particular value
+    assert age == 25
+
+    # check to see if the age is over 18
+    assert age > 18
+
+    # check to see if the age is under 100
+    assert age < 100
